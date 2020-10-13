b1b8829e721befb0f766867122496f3effb035a0
aldojaimejuarez <aldojaimejuarez@gmail.com>
2020-10-12 19:59:23 -0500

    Documento final: radicales

--- enumeracion.py
+++ enumeracion.py
@@ -1,15 +1,28 @@
 def run():
-    objetivo = int(input('Escoge un número entero y te regreso su raiz cuadrada exacta: '))
-    respuesta = 0
+    # objetivo = int(input('Escoge un número entero y te regreso su raiz cuadrada exacta: '))
+    # respuesta = 0
 
-    while respuesta**2 < objetivo:
-        respuesta += 1
+    # while respuesta**2 < objetivo:
+    #     respuesta += 1
     
-    if respuesta**2 == objetivo:
-        print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+    # if respuesta**2 == objetivo:
+    #     print(f'La raiz cuadrada de {objetivo} es {respuesta}')
 
-    else:
-        print(f'{objetivo} no tiene raiz cuadrada exacta')    
+    # else:
+    #     print(f'{objetivo} no tiene raiz cuadrada exacta')
+
+
+        objetivo = int(input('Escoge un número entero y te regreso su raiz cúbica exacta: '))
+        respuesta = 0
+
+        while respuesta**3 < objetivo:
+            respuesta += 1
+    
+        if respuesta**3 == objetivo:
+            print(f'La raiz cúbica de {objetivo} es {respuesta}')
+
+        else:
+            print(f'{objetivo} no tiene cúbica cuadrada exacta')
 
 if __name__ == '__main__':
     run()
