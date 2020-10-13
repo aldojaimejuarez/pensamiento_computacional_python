e50f264bdee2ea54167958d2760a20cce953c2a9
aldojaimejuarez <aldojaimejuarez@gmail.com>
2020-10-13 13:58:13 -0500

    reto none

---
+++ reto_nones.py
@@ -0,0 +1,11 @@
+def run():
+    impar = range(1, 100, 2)
+    # for i in impar:
+    #     print(i)
+    
+    for i in impar:
+        if i % 2 != 0:
+            print(i)
+
+if __name__ == '__main__':
+    run()
